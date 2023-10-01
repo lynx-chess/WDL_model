f5e9851068edcdc5975331b52d2707732d37da69
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-10-01 20:34:47 +0200

    Lynx 1578 update

--- run.sh
+++ run.sh
@@ -2,7 +2,10 @@
 
 # docker run -it --rm --mount type=bind,src=.,dst=/home/docker -w /home/docker python:3.11-slim bash
 
+apt-get update
+apt-get install -y build-essential
+
 pip install -r requirements.txt
 make
-scoreWDLstat -r
+./scoreWDLstat -r
 python scoreWDL.py --NormalizeToPawnValue 100 > ./wdl_output.txt
