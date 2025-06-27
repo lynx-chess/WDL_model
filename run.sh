4b54b3b39f728b3d6e5c3bbd5a932c889d1a519c
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2025-06-27 17:17:06 +0200

    Update WDL model after SF origin update (still 6489)

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 # docker run -it --rm --mount type=bind,src=.,dst=/home/docker -w /home/docker python:3.11-slim bash
 
 apt-get update
-apt-get install -y build-essential
+apt-get install -y build-essential libz-dev
 
 pip install -r requirements.txt
 make
 ./scoreWDLstat -r
-python scoreWDL.py --NormalizeToPawnValue 100 > ./wdl_output.txt
+python scoreWDL.py --NormalizeToPawnValue 100 --pngName WDL_model_summary.png > ./wdl_output.txt
